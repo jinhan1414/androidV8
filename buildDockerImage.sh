a373f02ff44ff5d6128f32813dc16921257b6a53
jinhan1414 <jinhan1414@gmail.com>
2020-03-23 11:41:09 +0800

    Update buildDockerImage.sh

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -1,12 +1,12 @@
 aliyun_username=xxxx
 aliyun_password=xxxx
-matrix.image-name=android-v8-build-env
-matrix.image-version=2.0
+image_name=android-v8-build-env
+image_version=2.0
 mkdir dock
 cd dock
 wget https://raw.githubusercontent.com/jinhan1414/androidV8/master/Dockerfile
 sudo docker login --username=${aliyun_username} registry.cn-chengdu.aliyuncs.com --password=${aliyun_password}
 docker images
-sudo docker build -t ${matrix.image-name}:${matrix.image-version} .
-sudo docker tag ${matrix.image-name}:${matrix.image-version} registry.cn-chengdu.aliyuncs.com/jinchengsong001/chengdu:${matrix.image-version}
-sudo docker push registry.cn-chengdu.aliyuncs.com/jinchengsong001/chengdu:${matrix.image-version}   
+sudo docker build -t ${image_name}:${image_version} .
+sudo docker tag ${image_name}:${image_version} registry.cn-chengdu.aliyuncs.com/jinchengsong001/chengdu:${image_version}
+sudo docker push registry.cn-chengdu.aliyuncs.com/jinchengsong001/chengdu:${image_version}   
