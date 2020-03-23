f0b81997c6ef2e04c73d66d1fcce4c096c272124
jinhan1414 <jinhan1414@gmail.com>
2020-03-23 15:11:51 +0800

    Update buildAndroidV8.sh

--- buildAndroidV8.sh
+++ buildAndroidV8.sh
@@ -1,6 +1,6 @@
 ./tools/dev/v8gen.py x64.release -vv
 # android-arm
-build_platform=android-arm
+build_platform=android-armeabi-v7a
 cp -r out.gn/x64.release out.gn/${build_platform}
 rm out.gn/${build_platform}/args.gn
 cp ../J2V8/v8/android-arm/args.gn  out.gn/${build_platform}/args.gn
@@ -9,7 +9,7 @@ cat out.gn/${build_platform}/args.gn
 touch out.gn/${build_platform}/args.gn
 
 # android-arm64
-build_platform=android-arm64
+build_platform=android-arm64-v8a
 cp -r out.gn/x64.release out.gn/${build_platform}
 rm out.gn/${build_platform}/args.gn
 cp ../J2V8/v8/android-arm64/args.gn  out.gn/${build_platform}/args.gn
@@ -27,7 +27,7 @@ cat out.gn/${build_platform}/args.gn
 touch out.gn/${build_platform}/args.gn
 
 # android-x64
-build_platform=android-x64
+build_platform=android-x86_64
 cp -r out.gn/x64.release out.gn/${build_platform}
 rm out.gn/${build_platform}/args.gn
 cp ../J2V8/v8/android-x64/args.gn  out.gn/${build_platform}/args.gn
@@ -36,12 +36,12 @@ cat out.gn/${build_platform}/args.gn
 touch out.gn/${build_platform}/args.gn
 
 # Build the V8 monolithic static liblary
-build_platform=android-arm
+build_platform=android-armeabi-v7a
 ninja -C out.gn/${build_platform} -t clean
 ninja -C out.gn/${build_platform} v8_monolith
 
 # Build the V8 monolithic static liblary
-build_platform=android-arm64
+build_platform=android-arm64-v8a
 ninja -C out.gn/${build_platform} -t clean
 ninja -C out.gn/${build_platform} v8_monolith
 
@@ -53,7 +53,7 @@ ninja -C out.gn/${build_platform} v8_monolith
 
 
 # Build the V8 monolithic static liblary
-build_platform=android-x64
+build_platform=android-x86_64
 ninja -C out.gn/${build_platform} -t clean
 ninja -C out.gn/${build_platform} v8_monolith
 
@@ -64,11 +64,11 @@ rm -rf libv8_monolith
 mkdir libv8_monolith
 cd libv8_monolith
 
-build_platform=android-arm
+build_platform=android-armeabi-v7a
 mkdir ${build_platform}
 cp ../v8/out.gn/${build_platform}/obj/v8_monolith.a ${build_platform}/v8_monolith.a
 
-build_platform=android-arm64
+build_platform=android-arm64-v8a
 mkdir ${build_platform}
 cp ../v8/out.gn/${build_platform}/obj/v8_monolith.a ${build_platform}/v8_monolith.a
 
@@ -76,7 +76,7 @@ build_platform=android-x86
 mkdir ${build_platform}
 cp ../v8/out.gn/${build_platform}/obj/v8_monolith.a ${build_platform}/v8_monolith.a
 
-build_platform=android-x64
+build_platform=android-x86_64
 mkdir ${build_platform}
 cp ../v8/out.gn/${build_platform}/obj/v8_monolith.a ${build_platform}/v8_monolith.a
 
