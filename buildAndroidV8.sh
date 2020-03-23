505bd97103888a4420291ca1d9d51641823dc14e
jinhan1414 <jinhan1414@gmail.com>
2020-03-23 10:33:07 +0800

    Update buildAndroidV8.sh

--- buildAndroidV8.sh
+++ buildAndroidV8.sh
@@ -1,6 +1,7 @@
+./tools/dev/v8gen.py x64.release -vv
 # android-arm
 build_platform=android-arm
-./tools/dev/v8gen.py ${build_platform} -vv
+cp -r out.gn/x64.release out.gn/${build_platform}
 rm out.gn/${build_platform}/args.gn
 cd out.gn/${build_platform}/
 wget https://raw.githubusercontent.com/jinhan1414/J2V8/master/v8/android-arm/args.gn
@@ -14,7 +15,7 @@ ninja -C out.gn/${build_platform} v8_monolith
 
 # android-arm64
 build_platform=android-arm64
-./tools/dev/v8gen.py ${build_platform} -vv
+cp -r out.gn/x64.release out.gn/${build_platform}
 rm out.gn/${build_platform}/args.gn
 cd out.gn/${build_platform}/
 wget https://raw.githubusercontent.com/jinhan1414/J2V8/master/v8/android-arm64/args.gn
@@ -28,7 +29,7 @@ ninja -C out.gn/${build_platform} v8_monolith
 
 # android-x86
 build_platform=android-x86
-./tools/dev/v8gen.py ${build_platform} -vv
+cp -r out.gn/x64.release out.gn/${build_platform}
 rm out.gn/${build_platform}/args.gn
 cd out.gn/${build_platform}/
 wget https://raw.githubusercontent.com/jinhan1414/J2V8/master/v8/android-ia32/args.gn
@@ -42,7 +43,7 @@ ninja -C out.gn/${build_platform} v8_monolith
 
 # android-x64
 build_platform=android-x64
-./tools/dev/v8gen.py ${build_platform} -vv
+cp -r out.gn/x64.release out.gn/${build_platform}
 rm out.gn/${build_platform}/args.gn
 cd out.gn/${build_platform}/
 wget https://raw.githubusercontent.com/jinhan1414/J2V8/master/v8/android-x64/args.gn
