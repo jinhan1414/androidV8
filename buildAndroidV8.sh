c32a70bb4245221c17daeaabae32fdfbd0fbbc48
jinhan1414 <jinhan1414@gmail.com>
2020-03-23 17:30:09 +0800

    Update buildAndroidV8.sh

--- buildAndroidV8.sh
+++ buildAndroidV8.sh
@@ -36,26 +36,30 @@ cat out.gn/${build_platform}/args.gn
 touch out.gn/${build_platform}/args.gn
 
 # Build the V8 monolithic static liblary
+echo "开始构建android-armeabi-v7a"
 build_platform=android-armeabi-v7a
-ninja -C out.gn/${build_platform} -t clean
-ninja -C out.gn/${build_platform} v8_monolith
+ninja -C -j 6 out.gn/${build_platform} -t clean
+ninja -C -j 6 out.gn/${build_platform} v8_monolith
 
 # Build the V8 monolithic static liblary
+echo "开始构建android-arm64-v8a"
 build_platform=android-arm64-v8a
-ninja -C out.gn/${build_platform} -t clean
-ninja -C out.gn/${build_platform} v8_monolith
+ninja -C -j 6 out.gn/${build_platform} -t clean
+ninja -C -j 6 out.gn/${build_platform} v8_monolith
 
 
 # Build the V8 monolithic static liblary
 build_platform=android-x86
-ninja -C out.gn/${build_platform} -t clean
-ninja -C out.gn/${build_platform} v8_monolith
+echo "开始构建android-x86"
+ninja -C -j 6 out.gn/${build_platform} -t clean
+ninja -C -j 6 out.gn/${build_platform} v8_monolith
 
 
 # Build the V8 monolithic static liblary
+echo "开始构建android-x86_64"
 build_platform=android-x86_64
-ninja -C out.gn/${build_platform} -t clean
-ninja -C out.gn/${build_platform} v8_monolith
+ninja -C -j 6 out.gn/${build_platform} -t clean
+ninja -C -j 6 out.gn/${build_platform} v8_monolith
 
 # 生成构建压缩包
 echo "构建完成，生成压缩包"
